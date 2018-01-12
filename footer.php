c40531216c87f9f0891b357973479883aca67acd
willfalcon <will@creativedistillery.com>
2018-01-11 21:19:50 -0600

    fixed copyright date function

--- footer.php
+++ footer.php
@@ -2,14 +2,14 @@
 
         <div class="col-12 col-md-6 d-flex flex-column align-items-center align-items-md-end order-md-2">
 
-          <p><a href="<?php echo get_field( 'privacy_policy_page', 'option' ); ?>">Privacy Policy</a></p>
-          <p><a href="<?php echo get_field( 'terms_conditions_page', 'option'); ?>">Terms & Conditions</a></p>
+          <p><a href="<?php the_field( 'privacy_policy_page', 'option' ); ?>">Privacy Policy</a></p>
+          <p><a href="<?php the_field( 'terms_conditions_page', 'option'); ?>">Terms & Conditions</a></p>
 
         </div>
 
         <div class="col-12 col-md-6 d-flex flex-column align-items-center align-items-md-start justify-content-center order-md-1">
 
-            <p>&copy<?php the_date( 'Y' ); ?> <?php bloginfo( 'name' ); ?></p>
+            <p>&copy<?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?></p>
 
         </div>
 
